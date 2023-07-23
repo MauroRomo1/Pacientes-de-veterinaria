ec46d9b2cbb41f549324aa67c03f8d82a019ba91
Mauro Romo <mauroromo.developer@outlook.com>
2023-07-23 17:09:00 -0300

    instalando sweetAlert2 y amplicando sus componentes en el formulario de citas

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -1,22 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import Swal from "sweetalert2";
 
 const AppointmentForm = () => {
+  const [formValues, setFormValues] = useState({
+    namePet: "",
+    nameOwner: "",
+    appointmentDate: "",
+    appointmentTime: "",
+    symptomsArea: "",
+  });
+
+  const { namePet, nameOwner, appointmentDate, appointmentTime, symptomsArea } =
+    formValues;
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      namePet.trim() === "" ||
+      nameOwner.trim() === "" ||
+      appointmentDate === "" ||
+      appointmentTime === "" ||
+      symptomsArea.trim() === ""
+    ) {
+      Swal.fire({
+        icon: "error",
+        title: "<h5>Debes de llenar todos los campos</h5>",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      Swal.fire({
+        icon: "success",
+        title: "<h5>Cita agendada</h5>",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
   return (
     <section className="container my-4">
       <Row className="justify-content-center ">
         <Col xs={11} md={10} lg={9} className="py-3 shadow formContainer">
           <h6>Llenar el formulario para crear una cita</h6>
           <hr />
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="namePet">
               <Form.Label>Nombre de la mascota</Form.Label>
-              <Form.Control type="text" name="namePet" />
+              <Form.Control
+                type="text"
+                name="namePet"
+                value={namePet}
+                required
+                onChange={handleChange}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="nameOwner">
               <Form.Label>Nombre de dueño/a</Form.Label>
-              <Form.Control type="text" name="nameOwner" />
+              <Form.Control
+                type="text"
+                name="nameOwner"
+                value={nameOwner}
+                required
+                onChange={handleChange}
+              />
             </Form.Group>
 
             <Row className="mb-3">
@@ -27,7 +81,13 @@ const AppointmentForm = () => {
                 controlId="appointmentDate"
               >
                 <Form.Label>Fecha</Form.Label>
-                <Form.Control type="date" name="appointmentDate" />
+                <Form.Control
+                  type="date"
+                  name="appointmentDate"
+                  value={appointmentDate}
+                  required
+                  onChange={handleChange}
+                />
               </Form.Group>
               <Form.Group
                 className="mb-3"
@@ -36,13 +96,23 @@ const AppointmentForm = () => {
                 controlId="appointmentTime"
               >
                 <Form.Label>Hora</Form.Label>
-                <Form.Control type="time" name="appointmentTime" />
+                <Form.Control
+                  type="time"
+                  name="appointmentTime"
+                  value={appointmentTime}
+                  required
+                  onChange={handleChange}
+                />
               </Form.Group>
             </Row>
             <Form.Group className="mb-3" controlId="symptomsArea">
               <Form.Label>Sintomas</Form.Label>
               <Form.Control
                 as="textarea"
+                name="symptomsArea"
+                value={symptomsArea}
+                required
+                onChange={handleChange}
                 rows={2}
                 placeholder="Sintomas de la mascota..."
               />
